bf852d42c5cacf3c2cc40a170da25a03a597b911
Kiran-G1 <kiran.gali11@gmail.com>
2018-05-23 11:22:31 +0530

    Add files via upload

---
+++ StandAloneBubbleSort.scala
@@ -0,0 +1,43 @@
+package com.kamali.exe
+import scala.util.matching.Regex
+
+import scala.io._
+import scala.util.matching._
+object StandAloneSort {
+  
+  def main(args:Array[String]):Unit=
+  {
+    
+    val source=scala.io.Source.fromFile("/home/kiran/shakespeare_words.txt")
+    
+    
+ 
+    val lines=source.getLines().filter(!_.isEmpty()).toArray
+    val refinedlines=lines.filter(!_.contains(",")).filter(!_.contains(".")).filter(!_.contains("'")).filter(!_.contains(";")).filter(!_.contains("?")).toArray
+    
+    
+  /* for ( i <-0 to refinedlines.length-1)
+   {
+     println(refinedlines.apply(i))
+   }
+   */
+
+    for ( i <-0 to refinedlines.length-1)
+    {
+      for(j<-0 to refinedlines.length-1)
+        if(refinedlines.apply(i).compareTo(refinedlines.apply(j))>0)
+            {
+            val temp= refinedlines.apply(i)
+            refinedlines(i)=refinedlines(j)
+            refinedlines(j)=refinedlines(i)
+          
+            }
+    }
+    
+   for ( k <-0 to refinedlines.length-1)
+   {
+     println(refinedlines.apply(k))
+   }
+  }
+  
+}
