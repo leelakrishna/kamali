22f8f7de8ca61191f2fdb92ff51016315b2c2f2c
Kiran-G1 <kiran.gali11@gmail.com>
2018-05-29 12:35:25 +0530

    Add files via upload

---
+++ StandAloneQuickSort.scala
@@ -0,0 +1,34 @@
+
+import scala.util.matching.Regex
+import scala.io._
+import scala.util.matching._
+object StandAloneMergeSort{
+  
+  def main(args:Array[String]):Unit=
+  {
+    
+    val source=scala.io.Source.fromFile("/home/kiran/shakespeare_words.txt")
+    val lines=source.getLines().filter(!_.isEmpty()).toList
+    val refinedlines=lines.filter(!_.contains(",")).filter(!_.contains(".")).filter(!_.contains("'")).filter(!_.contains(";")).filter(!_.contains("?")).toList
+    source.close()
+    
+ val sorted=QuickSort(refinedlines)
+    
+   for ( k <-0 to refinedlines.length-1)
+   {
+     println(sorted.apply(k))
+   }
+  }
+
+def QuickSort(lst:List[String]):List[String]=lst match{
+	
+	case Nil =>lst
+	case h::Nil=>lst
+	case pivot:: t=>
+		val(less,greater)=t.partition(_< pivot)
+		QuickSort(less)::: (pivot::QuickSort(greater))
+
+  }
+
+}
+
